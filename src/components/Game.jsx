7d70f4883ea422e035de0dd1c8e19dcaae6a1f45
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-15 12:24:03 -0300

    trabalando no css de Game.css e fix no css global

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,14 +2,45 @@ import './Game.css';
 
 export function Game({ verifyLetter }) {
   return (
-    <div>
-      <h1>Game</h1>
-      <button
-        type="button"
-        onClick={verifyLetter}
-      >
-        Finalizar o Jogo
-      </button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+
+      <h1>Advinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+
+      <div className="wordContainer">
+      <span className="letter">A</span>
+      <span className="letter"></span>
+        <span className="blackSquare"></span>
+      </div>
+
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input
+            type="text"
+            name="letter"
+            maxLength={1}
+            required
+          />
+
+          <button
+            type="button"
+          >
+            Jogar
+          </button>
+        </form>
+      </div>
+
+      <div className="wrongLetterContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   );
 }
